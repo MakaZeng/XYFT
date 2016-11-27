1cdd47de83f7f80d7e705ffeb7b22b603b4c154f
maka <makazeng@gmail.com>
2016-11-27 16:09:50 +0800

    mmm

--- Dao/DatabaseCreator.py
+++ Dao/DatabaseCreator.py
@@ -2,7 +2,7 @@ import MysqlDBManager as DBM
 import MysqlDBConfig as DBC
 
 def CreateTableHistoryIfNotEXist():
-    createSql = "CREATE TABLE IF NOTEXISTS {0} ( \
+    createSql = "CREATE TABLE IF NOT EXISTS {0} ( \
     {1} VARCHAR(45) NOT NULL, \
     {2} DATETIME NULL, \
     {3} VARCHAR(45) NOT NULL, \
@@ -23,7 +23,7 @@ def CreateTableHistoryIfNotEXist():
     DBM.maka_do_sql(createSql)
 
 def CreateTableTongjiIfNotEXist():
-    createSql = "CREATE TABLE IF NOTEXISTS {0} ( \
+    createSql = "CREATE TABLE IF NOT EXISTS {0} ( \
     {1} VARCHAR(45) NOT NULL, \
     {2} VARCHAR(1024*1024) NOT NULL, \
     PRIMARY KEY ({1}), \
@@ -33,5 +33,35 @@ def CreateTableTongjiIfNotEXist():
     COLLATE = utf8_bin;".format(DBC.TJTAB,DBC.TJQI,DBC.TJRS)
     DBM.maka_do_sql(createSql)
 
+def CreateTablePersonIfNotEXist():
+    createSql = "CREATE TABLE IF NOT EXISTS {0} ( \
+    {1} BIGINT(10) NOT NULL AUTO_INCREMENT, \
+    {2} VARCHAR(512) NOT NULL, \
+    {3} VARCHAR(45) NOT NULL, \
+    {4} VARCHAR(512) NOT NULL, \
+    {5} BIGINT(10) NOT NULL, \
+    {6} BIGINT(10) NOT NULL, \
+    PRIMARY KEY ({1}), \
+    UNIQUE INDEX `{1}_UNIQUE` (`{1}` ASC)) \
+    ENGINE = InnoDB \
+    DEFAULT CHARACTER SET = utf8 \
+    COLLATE = utf8_bin;".format(DBC.PSTAB,DBC.PSID,DBC.PSDEVICE,DBC.PSSTATUS,DBC.PSHEAD,DBC.PSMONEY,DBC.PSLEVEL)
+    DBM.maka_do_sql(createSql)
 
+def CreateTableBeatListIfNotEXist():
+    createSql = "CREATE TABLE IF NOT EXISTS {0} ( \
+    {1} BIGINT(10) NOT NULL AUTO_INCREMENT, \
+    {2} VARCHAR(45) NOT NULL, \
+    {3} DATETIME NOT NULL, \
+    {4} INT NOT NULL, \
+    {5} INT NOT NULL, \
+    {6} BIGINT(10) NOT NULL, \
+    {7} INT NOT NULL, \
+    PRIMARY KEY ({1}), \
+    UNIQUE INDEX `{1}_UNIQUE` (`{1}` ASC)) \
+    ENGINE = InnoDB \
+    DEFAULT CHARACTER SET = utf8 \
+    COLLATE = utf8_bin;".format(DBC.BLTAB,DBC.BLID,DBC.BLQI,DBC.BLTIME,DBC.BLROAD,DBC.BLNUMBER,DBC.BLMONEY,DBC.BLSTATUS)
+    DBM.maka_do_sql(createSql)
 
+CreateTablePersonIfNotEXist()
