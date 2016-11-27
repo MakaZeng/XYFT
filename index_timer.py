73c359abeba64411fc20008f933b0d18652a8dc3
maka <makazeng@gmail.com>
2016-11-27 23:46:50 +0800

    mmm

--- index_timer.py
+++ index_timer.py
@@ -19,42 +19,49 @@ beginURL = 'http://www.cp66607.com/api/xyft?act=index'
 
 content = NM.maka_simplegetcontent(beginURL)
 
+getTop = "select {0} from {1} ORDER BY {0} DESC LIMIT 1".format(CF.HISQI,CF.HISTAB)
+
+databaseTop = DBM.maka_do_sql(getTop)[0][0]
+
 if len(content):
     s = json.loads(content)
     first = s[0]
 
     qishu = first["xissue"]
-    shijian = float(first["xkjtime"]) + 60*60
-    shijian = DU.convertSecondsFrom1970ToDate(shijian)
+    if long(qishu) > long(databaseTop) :
+        shijian = float(first["xkjtime"]) + 60 * 60
+        shijian = DU.convertSecondsFrom1970ToDate(shijian)
 
-    n1 = first["xn1"]
-    n2 = first["xn2"]
-    n3 = first["xn3"]
-    n4 = first["xn4"]
-    n5 = first["xn5"]
-    n6 = first["xn6"]
-    n7 = first["xn7"]
-    n8 = first["xn8"]
-    n9 = first["xn9"]
-    n10 = first["xn10"]
+        n1 = first["xn1"]
+        n2 = first["xn2"]
+        n3 = first["xn3"]
+        n4 = first["xn4"]
+        n5 = first["xn5"]
+        n6 = first["xn6"]
+        n7 = first["xn7"]
+        n8 = first["xn8"]
+        n9 = first["xn9"]
+        n10 = first["xn10"]
 
-    #DBC.CreateTableHistoryIfNotEXist()
-    sql = "INSERT INTO {1}.{2} ({3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14}) VALUES ('{15}','{16}','{17}','{18}','{19}','{20}','{21}','{22}','{23}','{24}','{25}','{26}');".format('',CF.Database,CF.HISTAB,CF.HISQI,CF.HISTIME,CF.HISN1,CF.HISN2,CF.HISN3,CF.HISN4,CF.HISN5,CF.HISN6,CF.HISN7,CF.HISN8,CF.HISN9,CF.HISN10,qishu,shijian,n1,n2,n3,n4,n5,n6,n7,n8,n9,n10)
-    DBM.maka_do_sql(sql)
-    cm = CCM.CalculateManager()
-    cm.calculate()
-    json = demjson.encode(cm.results)
+        # DBC.CreateTableHistoryIfNotEXist()
+        sql = "INSERT INTO {1}.{2} ({3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14}) VALUES ('{15}','{16}','{17}','{18}','{19}','{20}','{21}','{22}','{23}','{24}','{25}','{26}');".format(
+            '', CF.Database, CF.HISTAB, CF.HISQI, CF.HISTIME, CF.HISN1, CF.HISN2, CF.HISN3, CF.HISN4, CF.HISN5,
+            CF.HISN6, CF.HISN7, CF.HISN8, CF.HISN9, CF.HISN10, qishu, shijian, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10)
+        DBM.maka_do_sql(sql)
+        cm = CCM.CalculateManager()
+        cm.calculate()
+        json = demjson.encode(cm.results)
 
-    #DBC.CreateTableTongjiIfNotEXist()
+        # DBC.CreateTableTongjiIfNotEXist()
 
-    sql = "DELETE FROM {0}.{1} WHERE {2} > 0;".format(CF.Database,CF.TJTAB,CF.TJQI)
-    DBM.maka_do_sql(sql)
-    print sql
+        sql = "DELETE FROM {0}.{1} WHERE {2} > 0;".format(CF.Database, CF.TJTAB, CF.TJQI)
+        DBM.maka_do_sql(sql)
+        print sql
 
+        sql = "INSERT INTO {0}.{1} ({2},{3}) VALUES ('{4}','{5}');".format(CF.Database, CF.TJTAB, CF.TJQI, CF.TJRS,
+                                                                           qishu, json)
+        DBM.maka_do_sql(sql)
 
-    sql = "INSERT INTO {0}.{1} ({2},{3}) VALUES ('{4}','{5}');".format(CF.Database,CF.TJTAB,CF.TJQI,CF.TJRS,qishu,json)
-    DBM.maka_do_sql(sql)
-
-    time.sleep(1)
-    yc = Yuce.Yuce()
-    yc.startYuce()
+        time.sleep(1)
+        yc = Yuce.Yuce()
+        yc.startYuce()
