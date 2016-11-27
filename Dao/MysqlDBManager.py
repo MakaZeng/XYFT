65283e5d7da76f80372f29c08f1a9c6f039ccbe6
maka <makazeng@gmail.com>
2016-11-27 12:03:48 +0800

    mmm

--- Dao/MysqlDBManager.py
+++ Dao/MysqlDBManager.py
@@ -4,7 +4,6 @@ import MysqlDBConfig as DBC
 
 def maka_do_sql(sql):
     try:
-        print sql
         conn = MySQLdb.connect(host=DBC.Host, user=DBC.Name, passwd=DBC.Password, port=DBC.Port, charset='utf8')
         cur = conn.cursor()
         conn.select_db(DBC.Database)
