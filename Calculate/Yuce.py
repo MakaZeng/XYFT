a10b36b0893319e63fd5a95cf36e2cc24a725045
maka <makazeng@gmail.com>
2016-11-27 19:20:45 +0800

    mmm

--- Calculate/Yuce.py
+++ Calculate/Yuce.py
@@ -28,17 +28,18 @@ class Yuce(object):
 
         type = self.getRandom()
 
-        insertsql = ''
+        insertsql = 'INSERT INTO {0}.{1} ({2},{3},{4},{5},{6},{7},{8}) \
+            VALUES '.format(DBC.Database, DBC.BLTAB, \
+            DBC.BLQI, DBC.BLTIME, DBC.BLROAD, DBC.BLNUMBER, DBC.BLMONEY,DBC.BLSTATUS, DBC.BLPERSON, )
 
         personID = 100
 
         numbers = type['numbers']
         for n in numbers:
-            insertsql = insertsql + "INSERT INTO {0}.{1} ({2},{3},{4},{5},{6},{7},{8}) \
-            VALUES ('{9}','{10}','{11}',{12},{13},{14},{15});".format( \
-            DBC.Database, DBC.BLTAB, \
-            DBC.BLQI, DBC.BLTIME, DBC.BLROAD, DBC.BLNUMBER, DBC.BLMONEY,DBC.BLSTATUS, DBC.BLPERSON, \
+            insertsql = insertsql + "('{0}','{1}','{2}',{3},{4},{5},{6}),".format( \
             str(long(top)+1),currentTime,type['road'],n,long(type['beat']),0,personID)
+
+        insertsql = insertsql[:-1]
         print insertsql
         DBM.maka_do_sql(insertsql)
 
