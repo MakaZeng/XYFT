27aeea10b2209f74a6594c49e34c1f9ae5076e1e
maka <makazeng@gmail.com>
2016-11-30 17:22:39 +0800

    mmm

--- Calculate/Yuce.py
+++ Calculate/Yuce.py
@@ -13,35 +13,36 @@ class Yuce(object):
 
     def startYuce(self):
 
+        CREATOR.CreateTablePersonIfNotEXist()
         CREATOR.CreateTableBeatListIfNotEXist()
 
-        sql = "select {0} from {1} order by {2} DESC LIMIT 1".format(DBC.HISQI,DBC.HISTAB, DBC.HISQI)
-        top = DBM.maka_do_sql(sql)
-        top = top[0][0]
-        longTop = long(top)
-        if longTop %180 == 0 :
-            next = DU.getNextDayZeroQishu(top)
-            top = next
-
-        currentTime = long(time.time())
-        currentTime = DU.convertSecondsFrom1970ToDate(currentTime)
-
-        type = self.getRandom()
-
-        insertsql = 'INSERT INTO {0}.{1} ({2},{3},{4},{5},{6},{7},{8}) \
-            VALUES '.format(DBC.Database, DBC.BLTAB, \
-            DBC.BLQI, DBC.BLTIME, DBC.BLROAD, DBC.BLNUMBER, DBC.BLMONEY,DBC.BLSTATUS, DBC.BLPERSON, )
-
-        personID = 100
-
-        numbers = type['numbers']
-        for n in numbers:
-            insertsql = insertsql + "('{0}','{1}','{2}',{3},{4},{5},{6}),".format( \
-            str(long(top)+1),currentTime,type['road'],n,long(type['beat']),0,personID)
-
-        insertsql = insertsql[:-1]
-        print insertsql
-        DBM.maka_do_sql(insertsql)
+        # sql = "select {0} from {1} order by {2} DESC LIMIT 1".format(DBC.HISQI,DBC.HISTAB, DBC.HISQI)
+        # top = DBM.maka_do_sql(sql)
+        # top = top[0][0]
+        # longTop = long(top)
+        # if longTop %180 == 0 :
+        #     next = DU.getNextDayZeroQishu(top)
+        #     top = next
+        #
+        # currentTime = long(time.time())
+        # currentTime = DU.convertSecondsFrom1970ToDate(currentTime)
+        #
+        # type = self.getRandom()
+        #
+        # insertsql = 'INSERT INTO {0}.{1} ({2},{3},{4},{5},{6},{7},{8}) \
+        #     VALUES '.format(DBC.Database, DBC.BLTAB, \
+        #     DBC.BLQI, DBC.BLTIME, DBC.BLROAD, DBC.BLNUMBER, DBC.BLMONEY,DBC.BLSTATUS, DBC.BLPERSON, )
+        #
+        # personID = 100
+        #
+        # numbers = type['numbers']
+        # for n in numbers:
+        #     insertsql = insertsql + "('{0}','{1}','{2}',{3},{4},{5},{6}),".format( \
+        #     str(long(top)+1),currentTime,type['road'],n,long(type['beat']),0,personID)
+        #
+        # insertsql = insertsql[:-1]
+        # print insertsql
+        # DBM.maka_do_sql(insertsql)
 
 
     def getRandom(self):
